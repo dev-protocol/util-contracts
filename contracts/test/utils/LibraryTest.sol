282e53350d321005e025d77a2ea0b433e2296e9d
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-08-16 16:05:44 +0900

    update by lint

---
+++ contracts/test/utils/LibraryTest.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MPL-2.0
+pragma solidity ^0.8.0;
+
+import {AddressLib} from "../../utils/AddressLib.sol";
+import {Base64} from "../../utils/Base64.sol";
+
+contract LibraryTest {
+	using Base64 for bytes;
+	using AddressLib for address;
+
+	function base64Encode(string memory data)
+		external
+		pure
+		returns (string memory)
+	{
+		return bytes(abi.encodePacked(data)).encode();
+	}
+
+	function addressToChecksumString(address account)
+		external
+		pure
+		returns (string memory asciiString)
+	{
+		return account.toChecksumString();
+	}
+}
