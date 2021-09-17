00c46b202d2d8670792651f96bcc9b563f5ee26c
0xkenta <kenta0558kiichi614@gmail.com>
2021-09-17 19:36:12 +0200

    use IAdressRegistry for L2

--- contracts/test/MockWithdraw.sol
+++ contracts/test/MockWithdraw.sol
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: MPL-2.0
 pragma solidity ^0.8.0;
 
-// prettier-ignore
-import {IAddressConfig} from "@devprotocol/protocol/contracts/interface/IAddressConfig.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract MockWithdraw {
-	IAddressConfig private config;
+import {IAddressRegistry} from "@devprotocol/protocol-l2/contracts/interface/IAddressRegistry.sol";
 
-	constructor(address _config) {
-		config = IAddressConfig(_config);
+contract MockWithdraw {
+	IAddressRegistry private registry;
+
+	constructor(address _registry) {
+		registry = IAddressRegistry(_registry);
 	}
 
 	function withdraw(address) external {
-		IERC20(config.token()).transfer(msg.sender, 10000000000000000000);
+		IERC20(registry.registries("Dev")).transfer(
+			msg.sender,
+			10000000000000000000
+		);
 	}
 }
