86a51ea2b4356fc4b4d74944e550693462987825
0xkenta <kenta0558kiichi614@gmail.com>
2021-09-19 20:44:55 +0200

    delete unnecessary files

--- contracts/config/IConfig.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MPL-2.0
-pragma solidity ^0.8.0;
-
-interface IConfig {
-	function set(string memory _key, address _value) external;
-
-	function get(string memory _key) external view returns (address);
-}
