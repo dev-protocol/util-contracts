123a08e170f349f71b4ab1b9bc307e57b46130c0
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-10-19 16:20:43 +0900

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,11 +2,12 @@
 import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-ethers'
 import '@typechain/hardhat'
+import { HardhatUserConfig } from 'hardhat/config'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const config = {
+const config: HardhatUserConfig = {
 	solidity: {
 		version: '0.8.4',
 		settings: {
@@ -20,7 +21,7 @@ const config = {
 		arbitrumRinkeby: {
 			url: `https://arbitrum-rinkeby.infura.io/v3/${process.env.INFURA_KEY!}`,
 			accounts: {
-				mnemonic: process.env.MNEMONIC,
+				mnemonic: process.env.MNEMONIC!,
 			},
 		},
 	},
